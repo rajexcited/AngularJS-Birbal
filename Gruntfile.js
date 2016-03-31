dd7365577e68a6bc004f6d64a6b5e8678413e85c
raj <rajexcited@gmail.com>
2016-03-31 07:58:30 -0400

    panel path updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
             // define the files to lint
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            files: ['Gruntfile.js', 'src/**/panel/js/*.js', 'test/**/*.js'],
             // configure JSHint (documented at http://www.jshint.com/docs/)
             options: {
                 // more options here if you want to override JSHint defaults
--- src/devtools/devToolsPage.js
+++ src/devtools/devToolsPage.js
@@ -9,7 +9,7 @@
     chrome.devtools.panels.create(
         'Angular Birbal',
         null, // No icon path
-        '/src/devtools/panel/partials/index.html',
+        '/src/panel/partials/index.html',
         null // no callback needed
     );
 
