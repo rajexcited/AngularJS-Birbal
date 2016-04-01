bb09336ab7023744908c098e9c1460d4b7e405ac
raj <rajexcited@gmail.com>
2016-03-31 20:10:58 -0400

    added, configured and developed grunt tasks to build extension for zip.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
         uglify: {
             options: {
                 // the banner is inserted at the top of the output
-                banner: '/*! AngularJS Birbal v<%= pkg.version %>  \n<%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                banner: '/*! <%= pkg.name%> v<%= pkg.version %>  | <%= grunt.template.today("dd-mm-yyyy") %> | <%= pkg.homepage%> */\n'
             },
             dist: {
                 files: {
@@ -39,15 +39,86 @@ module.exports = function (grunt) {
                     'dist/content-script/inject/angularinspector.min.js': ['src/content-script/inject/angularinspector.js']
                 }
             }
+        },
+        copy: {
+            main: {
+                files: [
+                    // src files
+                    // copy all minified in dist to src and rename to .js
+                    {expand: true, cwd: 'dist/', src: '**/*.min.js', dest: 'zip/src/', ext: '.js'},
+                    {expand: true, cwd: 'src/panel/css/', src: '*.css', dest: 'zip/src/panel/'},
+                    {src: 'src/panel/partials/*.html', dest: 'zip/'},
+                    {src: 'src/devtools/devToolsPage.html', dest: 'zip/src/devtools/devToolsPage.html'},
+                    {src: 'src/devtools/devToolsPage.js', dest: 'zip/src/devtools/devToolsPage.js'},
+                    // node-modules/lib files
+                    // angular
+                    {src: 'node_modules/angular/angular.min.js', dest: 'zip/lib/angular/angular.js'},
+                    // jquery
+                    {src: 'node_modules/jquery/dist/jquery.js', dest: 'zip/lib/jquery/jquery.js'},
+                    // admin-lte
+                    {
+                        expand: true,
+                        cwd: 'node_modules/admin-lte/',
+                        flatten: true,
+                        src: ['bootstrap/js/bootstrap.min.js', 'dist/js/app.min.js'],
+                        dest: 'zip/lib/admin-lte/js/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'node_modules/admin-lte/',
+                        flatten: true,
+                        src: ['bootstrap/css/bootstrap.min.css', 'dist/css/AdminLTE.min.css', 'dist/css/skins/skin-blue.min.css'],
+                        dest: 'zip/lib/admin-lte/css/'
+                    },
+                    // ion-rangeslider
+                    {
+                        src: 'node_modules/ion-rangeslider/js/ion.rangeSlider.min.js',
+                        dest: 'zip/lib/ion-rangeslider/js/ion.rangeSlider.min.js'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'node_modules/ion-rangeslider/css/',
+                        src: ['ion.rangeSlider.skinNice.css', 'ion.rangeSlider.css', 'normalize.css'],
+                        dest: 'zip/lib/ion-rangeslider/css/'
+                    },
+                    // floathead
+                    {
+                        src: 'node_modules/floatthead/dist/jquery.floatThead.min.js',
+                        dest: 'zip/lib/floatthead/jquery.floatThead.min.js'
+                    }
+                ]
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    archive: 'zip/extension.zip'
+                },
+                files: [
+                    {expand: true, cwd: 'zip/', src: ['**/*'], dest: '/'},
+                    {src: ['lib/**', 'img/*'], dest: '/'}
+                ]
+            }
+        },
+        clean: {
+            dist: ['dist/**'],
+            compress: ['zip/**']
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
+    // time for each task
+    require('time-grunt')(grunt);
+
+    // Load the plugins to run below registered tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
     // Default task(s).
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('default', ['clean:dist', 'jshint', 'concat', 'uglify']);
+    grunt.registerTask('build-extension', ['default', 'clean:compress', 'copy', 'compress']);
 
 };
