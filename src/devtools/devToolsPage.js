934784448f1d97e81484a623c03b706548a3e561
raj <rajexcited@gmail.com>
2016-04-16 23:39:09 -0400

    element side panel

--- src/devtools/devToolsPage.js
+++ src/devtools/devToolsPage.js
@@ -3,6 +3,43 @@
     'use strict';
 
     console.log('devtoolsPage.js is loading.');
+
+    var getScopeContents = function () {
+        if (!window.angular || !$0) {
+            return 'Not angular Element';
+        }
+        var scope = window.angular.element($0).scope();
+        // Export $scope to the console
+        window.$scope = scope;
+        return (function (scope) {
+            var props, i,
+                scopeContents = {
+                    __ngPrivate__: {}
+                };
+
+            if (scope) {
+                props = Object.keys(scope);
+                for (i = 0; i < props.length; i++) {
+                    if (props[i].substring(0, 2) === '$$') {
+                        scopeContents.__ngPrivate__[props[i]] = scope[props[i]];
+                    } else {
+                        scopeContents[props[i]] = scope[props[i]];
+                    }
+                }
+            }
+            return scopeContents;
+        }(scope));
+    };
+
+    chrome.devtools.panels.elements.createSidebarPane('ng-properties', function (sidebar) {
+        // sidebar property eval
+        var updateElementProperties = function () {
+            sidebar.setExpression('(' + getScopeContents.toString() + ')()', '$scope');
+        };
+        updateElementProperties();
+        chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
+    });
+
     /////////////////////////////////////////////////////////
     //            Create Panel
     /////////////////////////////////////////////////////////
